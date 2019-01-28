c315bb06999d672f49917e966da90702086476bb
bauhuasbadguy <bauhaus.bad.guy@gmail.com>
2019-01-28 21:19:07 +0000

    first fully working version

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import json
 import time
 from datetime import datetime
-from flask import Flask, Markup, render_template
+from flask import Flask, request, render_template, Response, redirect
 import MySQLdb
 import datetime
 
 import threading
 import ping_tools as pt
+import logging
 
+logger = logging.getLogger("down_detector_logs")
+
+hdlr = logging.FileHandler("./logging/down_detection_logs.log")
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.INFO)
+
+
+logger.info("STARTING COLLECTION")
 ping_urls = {'google':'google.co.uk', 'google_DNS_server':'8.8.8.8',
         'Virgin_media':'virginmedia.com'}
 
+#start collecting PING data in a seperate thread
 storer = pt.ping_capturer('down', 'virgin', 'localhost', 'pingstore', ping_urls)
 
 thread = threading.Thread(target=storer.continuous_capture)
@@ -23,14 +35,43 @@ app = Flask(__name__)
  
 @app.route("/")
 def index():
-    return "Index!"
- 
-@app.route("/bars")
-def bars():
-    return "bars"
+    return redirect("/lines/recent")
+
+@app.route("/download_all_data")
+def data_dump():
+
+    logger.info("ALL DATA WAS REQUESTED BY {0}".format(request.remote_addr))
+
+    grab_cmd = "SELECT * from storedpings"
+    
+    db = MySQLdb.connect("localhost", "down", "virgin", "pingstore")
+    curs = db.cursor()
+
+    curs.execute(grab_cmd)
+
+    out_string = "Time,Event,SentPings,RecevedPings,LostPings"
+    out_string += ",MinPingTime,MaxPingTime,MeanPingTime"
+
+    #fetch data from the sql server and dump it into a csv
+    for rowi, row in enumerate(curs.fetchall()):
+        
+        out_string += '\n' + str(row[0])
+        for i in range(len(row)-1):
+            out_string += ',' + str(row[i+1])
+
+    return Response(out_string,
+                    mimetype="text/csv",
+                    headers={"Content-disposition":
+                             "attachment; filename=all_pinging_data_{0}.csv".format(time.strftime("%Y_%m_%d-%H_%M_%S"))})
 
 @app.route("/lines/<period>")
 def line_shower(period):
+
+    print("GOT A REQUEST")
+    print(request.remote_addr)
+    print("=======================")
+
+    logger.info("{0} graph was requested by {1}".format(period, request.remote_addr))
 
     #set up for the period we're interested in
     if period == 'day':
@@ -56,8 +97,8 @@ def line_shower(period):
 
     grab_cmd = "SELECT * from storedpings WHERE time >= now()- INTERVAL {0};".format(sql_period)
     
-    print(grab_cmd)
-    print("========================")
+    #print(grab_cmd)
+    #print("========================")
     #grab the data from SQL, reconnect each time to get the latest data
     db = MySQLdb.connect("localhost", "down", "virgin", "pingstore")
     curs = db.cursor()
@@ -69,20 +110,23 @@ def line_shower(period):
                 'backgroundColor': "rgba(0,0,220,0.2)",
                 'pointBorderColor': "rgba(0,0,220,1)",
                 'pointBackgroundColor': "rgba(0,0,220,1)",
-                'bezierCurve' : False,
+                'bezierCurve' : 'false',
+                'lineTension': 0,
                 'data':[]},
                 {'label':'Google DNS pings',
                 'borderColor': "rgba(220,0,0,1)",
                 'backgroundColor': "rgba(220,0,0,0.2)",
                 'pointBorderColor': "rgba(220,0,0,1)",
                 'pointBackgroundColor': "rgba(220,0,0,1)",
-                'bezierCurve' : False,
+                'bezierCurve' : 'false',
+                'lineTension': 0,
                 'data':[]},{'label':'Virgin media pings',
                 'borderColor': "rgba(0,220,0,1)",
                 'backgroundColor': "rgba(0,220,0,0.2)",
                 'pointBorderColor': "rgba(0,220,0,1)",
                 'pointBackgroundColor': "rgba(0,220,0,1)",
-                'bezierCurve' : False,
+                'bezierCurve' : 'false',
+                'lineTension': 0,
                 'data':[]}]
 
     #run through the grabbed data and plot it on a graph
@@ -122,7 +166,7 @@ def line_shower(period):
             factor = len(dataset['data'])//max_datapoints
             out_list = []
 
-            for i, point in dataset['data']:
+            for i, point in enumerate(dataset['data']):
             
                 if i % factor == 0:
                 
@@ -138,12 +182,13 @@ def line_shower(period):
                            time_units=plot_period,
                            day_link='/lines/day',
                            month_link='/lines/month',
-                           year_link='/lines/year')
+                           year_link='/lines/year',
+                           download_link='/download_all_data')
 
 
 #estabish the connection to the database
 #db = MySQLdb.connect("localhost", "down", "virgin", "pingstore")
-
+logger.info("STARTING FLASK SERVER")
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0', debug=True)
